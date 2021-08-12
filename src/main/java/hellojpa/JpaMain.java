83e042c2e0ca8f0c6546f1b3be722c15f74ea9d2
choi <hyeongwoo26@gmail.com>
2021-08-12 13:54:26 +0900

    영속성 컨텍스트

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,10 +1,11 @@
 package hellojpa;
 
+import org.hibernate.mapping.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
 
@@ -19,14 +20,14 @@ public class JpaMain {
 
         try {
 
-//            Member findMember = em.find(Member.class, 2L);
+            //비영속
+            Member member = new Member();
+            member.setId(100L);
+            member.setName("HelloJPA");
 
-            List<Member> result = em.createQuery("select m from Member as m ", Member.class)
-                    .getResultList();
-
-            for (Member member : result){
-                System.out.println("member.name = " + member.getName());
-            }
+            //영속
+            System.out.println("member = " + member);
+            em.persist(member);
 
             tx.commit(); //트랜잭션 커밋
 
